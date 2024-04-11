c1824c8ae1ce851fa562b93b85e2ded2e18e1bfb
Sujal <sujalkesharwani220@gmail.com>
2024-04-11 16:02:51 +0530

    add searchInput component

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,42 @@ import React, { useContext, useState } from "react";
 import Searchicon from "../assets/search-icon.svg";
 import { Cryptodata } from "./Context/Data";
 
+const Searchinput = ({ handleSearch }) => {
+  const [Text, setText] = useState("");
+  const { searchData, setcoinData, setsearchData } = useContext(Cryptodata);
+
+  const setcoindata = (coin) => {
+    setcoinData(coin);
+    setText("");
+    setsearchData();
+  };
+  return (
+    <>
+      <form className="flex ml-2">
+        <input
+          type="text"
+          placeholder="Search here.."
+          className="bg-gray-200 md:w-96 mx-auto sm:w-96 w-80 rounded-md text-green placeholder:text-gray-100 pl-2 required md:border border-transparent  focus:border-green"
+          onChange={(e) => {
+            e.preventDefault();
+            setText(e.target.value);
+            let query = e.target.value;
+            handleSearch(query);
+          }}
+          value={Text}
+        />
+        <button type="submit">
+          <img
+            src={Searchicon}
+            className="relative right-6 cursor-pointer"
+            alt="Search"
+          />
+        </button>
+      </form>
+    </>
+  );
+};
+
 const Search = () => {
   let { fetchsearchdata } = useContext(Cryptodata);
   let lodashfunc = debounce(function (val) {
