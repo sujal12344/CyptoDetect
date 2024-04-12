6e6efadcc957f2e5a0360af4789691e1b680cb02
Sujal <sujalkesharwani220@gmail.com>
2024-04-12 16:02:51 +0530

    based on text length show loading

--- src/components/Search.js
+++ src/components/Search.js
@@ -35,6 +35,32 @@ const Searchinput = ({ handleSearch }) => {
           />
         </button>
       </form>
+
+      {Text.length > 0 ? (
+        <ul className="w-96 p-2 bg-gray-100 backdrop-blur-md bg-opacity-60 absolute top-10 left-2 h-96 overflow-x-hidden rounded z-10 font-nunito text-white  ">
+          {searchData ? (
+            searchData.map((coin) => {
+              return (
+                <li
+                  className="flex mb-1 cursor-pointer "
+                  onClick={() => {
+                    setcoindata(coin.id);
+                  }}
+                  key={coin.id}
+                >
+                  <img src={coin.thumb} alt="" className="w-6 ml-2  h-6" />
+                  <span className="ml-2">{coin.name}</span>
+                </li>
+              );
+            })
+          ) : (
+            <div className="flex items-center justify-center w-full h-full">
+              <div className="border-4 rounded-full border-b-gray-200 animate-spin border-green h-12 w-12"></div>
+              <h2 className="ml-2 font-nunito">Searching</h2>
+            </div>
+          )}
+        </ul>
+      ) : null}
     </>
   );
 };
