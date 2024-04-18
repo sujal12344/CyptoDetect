c1cd4b2c0cc505b1f596c28716f2919d9a8b0935
Sujal <sujalkesharwani220@gmail.com>
2024-04-18 15:12:11 +0530

    Logo component added

---
+++ src/components/Logo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logoimg from "../assets/logo.svg";
+
+const Logo = () => {
+  return (
+    <div className="max-h-full max-w-full absolute top-7  left-10 flex items-center flex-row  ">
+      <Link to="/ ">
+        <img src={Logoimg} alt="" className="w-10" />
+      </Link>
+      <div className="text-lg font-nunito  text-[#90EE90]">CryptoDetect</div>
+    </div>
+  );
+};
+
+export default Logo;
