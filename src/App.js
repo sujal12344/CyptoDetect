7992933148c013c80a814e9f2530034429b89828
Sujal <sujalkesharwani220@gmail.com>
2024-05-14 01:02:51 +0530

    added app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Crypto from "./components/pages/Crypto";
+import Trending from "./components/pages/Trending";
+import Favourites from "./components/pages/Favourites";
+import CryptoDetails from "./components/CryptoDetails";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="/" element={<Crypto />}>
+            <Route path="/:Coinid" element={<CryptoDetails />} />
+          </Route>
+          <Route path="/trending" element={<Trending />}>
+            <Route path="/trending/:Coinid" element={<CryptoDetails />} />
+          </Route>
+          <Route path="/favourites" element={<Favourites />}>
+            <Route path="/favourites/:Coinid" element={<CryptoDetails />} />
+          </Route>
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
