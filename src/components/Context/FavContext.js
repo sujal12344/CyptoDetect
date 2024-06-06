3a362fe1ab670c36bfeb8adef0c62128a17e0542
Sujal <sujalkesharwani220@gmail.com>
2024-06-06 15:12:11 +0530

    setCoin functionality added

--- src/components/Context/FavContext.js
+++ src/components/Context/FavContext.js
@@ -10,7 +10,26 @@ import { Cryptodata } from "./Data";
 export const FavouriteContext = createContext({});
 
 export const FavouriteData = ({ children }) => {
+  const [allcoins, Setallcoins] = useState([]);
+
+  const setCoin = async (Coinid) => {
+    const oldcoin = await JSON.parse(localStorage.getItem("coins"));
+    if (oldcoin.includes(Coinid)) {
+      return null;
+    } else {
+      const Newcoin = [...oldcoin, Coinid];
+      localStorage.setItem("coins", JSON.stringify(Newcoin));
+      Setallcoins(Newcoin);
+    }
+  };
+
   return (
-    <FavouriteContext.Provider value={{}}>{children}</FavouriteContext.Provider>
+    <FavouriteContext.Provider
+      value={{
+        setCoin,
+      }}
+    >
+      {children}
+    </FavouriteContext.Provider>
   );
 };
